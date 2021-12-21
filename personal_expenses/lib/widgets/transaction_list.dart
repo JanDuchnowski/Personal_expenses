0717e5809d018bfcc4fe10cf804d97391711ac46
JanDuchnowski <tomex2001@gmail.com>
2021-12-21 03:34:24 +0100

    responsive chart and list

--- personal_expenses/lib/widgets/transaction_list.dart
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -10,67 +10,64 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions, this.deleteTransaction);
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 400,
-        child: transactions.isEmpty
-            ? Column(
-                children: [
-                  Text(
-                    "No transactions added yet!",
-                    style: TextStyle(
-                      fontFamily: 'Quicksand',
-                      fontWeight: FontWeight.bold,
-                    ),
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                "No transactions added yet!",
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 200,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/waiting.png",
+                    fit: BoxFit.cover,
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 200,
-                    child: Center(
-                      child: Image.asset(
-                        "assets/images/waiting.png",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 5.0,
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                            child: Text(
-                              '\$${transactions[index].amount}',
-                            ),
-                          ),
+                ),
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 5.0,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text(
+                          '\$${transactions[index].amount}',
                         ),
                       ),
-                      title: Text(transactions[index].title,
-                          style: Theme.of(context).textTheme.subtitle1),
-                      subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () =>
-                            deleteTransaction(transactions[index].id),
-                      ),
                     ),
-                  );
-                },
-                itemCount: transactions.length,
-              ));
+                  ),
+                  title: Text(transactions[index].title,
+                      style: Theme.of(context).textTheme.subtitle1),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTransaction(transactions[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
